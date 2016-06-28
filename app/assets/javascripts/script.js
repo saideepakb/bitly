8e0c97d927150b1b3186cf6a8cb4bccbd7bcdaab
Deepak Bhimaraju <dbhimaraju@covermymeds.com>
2016-06-28 16:29:43 -0400

    added bootstrap and destroy

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -13,19 +13,37 @@ function ($) {
   });
 
 	var ItemView = Backbone.View.extend({
-    tagName: 'li',
+    tagName: 'tr',
+    events: {
+      'click #destroy': 'destroy'
+    },
     initialize: function(){
-    _.bindAll(this, 'render'); // every function that uses 'this' as the current object should be in here
+    _.bindAll(this, 'render', 'destroy'); // every function that uses 'this' as the current object should be in here
     },
     render: function(){
 					console.log("B");
-          $(this.el).html('<span>'+this.model.get('link')+'     '+this.model.get('hash_val')+'</span>');
+          var originalUrl = '<th>' + this.model.get('link') + '</th>';
+          var shortUrl = '<th>' + '<a id="link" href="http://localhost:3000/r/' + this.model.get('hash_val')+'" role="button" class="btn btn-success">Link</a>' + '</th>';
+          var clickCount = '<th>' + '<a href="#" id="destroy" role="button" class="btn btn-warning">Destroy</a>' + '</th>';
+          $(this.el).html( originalUrl + shortUrl + clickCount );
           return this;
+    },
+    destroy: function(){
+      var response = window.confirm("Are you sure you want to delete " + this.model.get('link'));
+      if(response == true){
+          this.model.destroy({
+            success: function(model, response){
+              window.alert('Model successfully deleted');
+            }
+          });
+      } else{
+        return false;
+      }
     }
   });
 
 	var AppView = Backbone.View.extend({
-    el: $('#app'), // el attaches to existing element
+    el: $('.container'), // el attaches to existing element
 		events: {
   		'keyup #urlInput': 'processKey'
 		},
@@ -53,7 +71,7 @@ function ($) {
       var itemView = new ItemView({
         model: item
       });
-      $('ul', this.el).append(itemView.render().el);
+      $('table', this.el).append(itemView.render().el);
     },
 		processKey: function(e) {
 
