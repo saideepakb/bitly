1313df13235c94f795853e67cc6a36458db8c1f0
Deepak Bhimaraju <dbhimaraju@covermymeds.com>
2016-07-17 12:56:25 -0400

    using postgres for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160620204028) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string   "link"
     t.string   "hash_val"
